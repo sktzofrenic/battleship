56c9e2f49e71e65cf3008a16328e2623c4350ca9
sktzofrenic <dan@danwins.com>
2017-12-07 13:14:42 -0500

    Project setup complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,10 +9,6 @@ require('jquery');
 require('font-awesome-webpack');
 require('bootstrap');
 
-// Your own code
-require('./plugins.js');
-require('./script.js');
-
 import Vue from 'vue'
 import App from './App.vue'
 
