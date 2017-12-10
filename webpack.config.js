45f5626b37eb750cc44acf55daac253501d25562
sktzofrenic <dan@danwins.com>
2017-12-09 23:26:59 -0500

    Update Heroku build

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ const ManifestRevisionPlugin = require('manifest-revision-webpack-plugin');
 const debug = (process.env.NODE_ENV !== 'production');
 
 // Development asset host (webpack dev server)
-const publicHost = debug ? 'http://myground.org:5001' : '';
+const publicHost = debug ? '/' : '';
 
 const rootAssetPath = path.join(__dirname, 'assets');
 
